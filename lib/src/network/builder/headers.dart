1d1d4711f9985a1e5bd509ffa7d4cd0b7b4a28ea
Deven Joshi <deven9852@gmail.com>
2023-08-16 01:48:44 +0530

    added http headers and dep changes

--- lib/src/network/builder/headers.dart
+++ lib/src/network/builder/headers.dart
@@ -9,6 +9,10 @@ class HeaderBuilder {
     final Map<String, String> map = {};
 
     map.addEntries({
+      MapEntry("Access-Control-Allow-Origin", "*"), // Required for CORS support to work
+      MapEntry("Access-Control-Allow-Credentials", "true"), // Required for cookies, authorization headers with HTTPS
+      MapEntry("Access-Control-Allow-Headers", "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale"),
+      MapEntry("Access-Control-Allow-Methods", "POST, OPTIONS"),
       _buildAuthorizationsHeader(),
       if (includeContentType) _buildContentType(),
     });
